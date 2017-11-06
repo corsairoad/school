644634f07a43c7804ab51f590c7e1c1e48ed78c9
fadli <fadlimunandar99@gmail.com>
2017-11-06 15:12:14 +0700

    adding auth2

--- src/main/java/com/fm/school/config/SecurityConfig.java
+++ src/main/java/com/fm/school/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //super.configure(http);
-        http.authorizeRequests().antMatchers("/school/**").authenticated();
+        http.authorizeRequests().antMatchers("/**").authenticated();
     }
 
     @Override
---
+++ src/main/java/com/fm/school/config/WebSecurityInit.java
@@ -0,0 +1,9 @@
+package com.fm.school.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by fadlymunandar on 11/4/17.
+ */
+public class WebSecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
